dad0549114bc782895580a444ec5ebc667b86493
GodWine <xieyutong_king@163.com>
2020-04-14 22:47:26 +0800

    Monitor Tracker - introduce forkjoin

--- src/test/java/com/huitong/monitortracker/MonitorTrackerApplicationTests.java
+++ src/test/java/com/huitong/monitortracker/MonitorTrackerApplicationTests.java
@@ -1,12 +1,12 @@
 package com.huitong.monitortracker;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class MonitorTrackerApplicationTests {
 
-	@Test
+//	@Test
 	void contextLoads() {
 	}
 
