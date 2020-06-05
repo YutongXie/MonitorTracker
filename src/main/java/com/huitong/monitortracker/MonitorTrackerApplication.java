7a40763150302535c5ba909040275f61401059fb
GodWine <xieyutong_king@163.com>
2020-06-05 21:29:18 +0800

    Monitor Tracker - split log file base on job name

--- src/main/java/com/huitong/monitortracker/MonitorTrackerApplication.java
+++ src/main/java/com/huitong/monitortracker/MonitorTrackerApplication.java
@@ -1,10 +1,14 @@
 package com.huitong.monitortracker;
 
+import org.apache.commons.lang3.ArrayUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 @ComponentScan
@@ -13,7 +17,13 @@ import org.springframework.context.annotation.ComponentScan;
 public class MonitorTrackerApplication {
 
 	public static void main(String[] args) {
+		if(ArrayUtils.isNotEmpty(args)) {
+			Arrays.stream(args).forEach(arg ->{
+				if(StringUtils.trimToEmpty(arg).startsWith("--log.name=")){
+					System.setProperty("log.name", arg.substring(arg.indexOf('=') + 1));
+				}
+			});
+		}
 		SpringApplication.run(MonitorTrackerApplication.class, args);
 	}
-
 }
